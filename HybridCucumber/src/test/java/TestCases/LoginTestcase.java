01af86fc961fe4474c495c4a914748d02f4fb91f
divyak <divyak@NICDM158.nousinfo.com>
2022-02-02 17:09:39 +0530

    hybridcucumber second commit

--- HybridCucumber/src/test/java/TestCases/LoginTestcase.java
+++ HybridCucumber/src/test/java/TestCases/LoginTestcase.java
@@ -1,5 +1,36 @@
 package TestCases;
 
-public class LoginTestcase {
+import java.io.IOException;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import Baseclass.Libraryclass;
+import Pages.Loginpage;
+import SeleniumReuseableFunction.Seleniumutility;
+
+public class LoginTestcase extends Libraryclass {
+	Seleniumutility selenium;
+	Loginpage lp;
+	@BeforeMethod
+	public void openapp()
+	{
+	launchapp("chrome","https://opensource-demo.orangehrmlive.com/");
+	}
+	@Test
+	public void login()
+	{
+	lp=new Loginpage(driver);
+	lp.login("Admin","admin123");
+	}
+
+	@AfterMethod
+	public void teardown() throws IOException
+	{
+	selenium=new Seleniumutility(driver);
+	selenium.screenshot("C:\\Users\\divyak\\git\\HybridCucumber\\HybridCucumber\\src\\test\\resources\\Screenshorts\\login.png");
+	closeapp();
+	}
 
 }
